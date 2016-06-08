c417cbcd676ea277d735a9ea1501788c165f8184
Willyb15 <william.steen.bryant@gmail.com>
2016-06-07 21:59:17 -0400

    updated a lot

--- front-end/js/controller.js
+++ front-end/js/controller.js
@@ -16,12 +16,13 @@ myApp.config(function($routeProvider, $locationProvider){
 	}).when('/login',{
 		templateUrl: 'views/login.html',
 		controller: 'myController'
+	}).when('/start',{
+		templateUrl: 'views/start.html',
+		controller: 'gameController'
 	})
 });
 
-
 myApp.controller('myController',function($scope, $http,$location, $cookies, $sce){
-
 	$scope.$watch(function() { 
 	    return $location.path(); 
 	    },
@@ -47,7 +48,7 @@ myApp.controller('myController',function($scope, $http,$location, $cookies, $sce
 				$scope.message = "Please re-enter password.";
 			}else{
 				$cookies.put('username', $scope.username);
-				$location.path('/game')
+				$location.path('/start')
 			}
 		},function errorCallback(response){
 			console.log("error");
@@ -67,7 +68,7 @@ myApp.controller('myController',function($scope, $http,$location, $cookies, $sce
 					$scope.message = "Username already in use.  Please choose another username.";
 				}else{
 					$cookies.put('username', username);
-					$location.path('/game');
+					$location.path('/start');
 				}
 			},function errorCallback(response){
 				console.log("error");
@@ -84,26 +85,63 @@ myApp.controller('myController',function($scope, $http,$location, $cookies, $sce
 });
 
 myApp.controller('gameController',function($scope, $http,$location){
-	var playerOne = true;
-	var playerTwo = false;
-	var playerOnePosition = 0;
-	var playerTwoPosition = 0;
-	var playerOneBank = 500;
-	var playerTwoBank = 500;
-
-	$scope.message = "Lets Play Monopoly! Player One First";
+	window.playerOnePosition = 0;
+	window.playerTwoPosition = 0;
+	window.playerOneBank = 500;
+	window.playerTwoBank = 500;
+	var onePlayer;
+	var twoPlayer;
+	//for development
+	var playerOneTurn;
+	var playerTwoTurn;
+	var playerOneProperties = [];
+	var playerTwoProperties = [];
+	$scope.playerOneProperties = playerOneProperties;
+	$scope.playerTwoProperties = playerTwoProperties;
 	$scope.playerOneBank = playerOneBank;
 	$scope.playerTwoBank = playerTwoBank;
 
-	var changePlayer = function(){
-		if(playerOne){
-			$scope.message = "Player Two Turn";
-			playerOne = false;
-			playerTwo = true;
+	$scope.onePlayerGame = function(){
+		onePlayer = true;
+		twoPlayer = false;
+		$scope.whoRollsFirst = "Please roll to see who's first";
+		whosFirst();
+		console.log("playing with the computer");
+	}
+
+	$scope.twoPlayerGame = function(){
+		onePlayer = false;
+		twoPlayer = true;
+		$scope.roll = true;
+	}
+
+	$scope.whosFirst = function(){
+		var playerOneTotal =  Math.floor(Math.random() * 6 + 1) + Math.floor(Math.random() * 6 + 1);
+		var playerTwoTotal =  Math.floor(Math.random() * 6 + 1) + Math.floor(Math.random() * 6 + 1);
+		$scope.total = true;
+		$scope.playerOneTotal = playerOneTotal;
+		$scope.playerTwoTotal = playerTwoTotal;
+
+		if(playerOneTotal > playerTwoTotal){
+			playerOneTurn = true;
+			playerTwoTurn = false;
 		}else{
-			$scope.message = "Player One Turn";
-			playerTwo = false;
-			playerOne = true;
+			playerOneTurn = false;
+			playerTwoTurn = true;
+		}
+		$scope.play = true;
+	}
+	$scope.playNow = function(){
+		$location.path('/game');
+	}
+	var changePlayer = function(){
+		if(playerOneTurn){
+			playerOneTurn = false;
+			playerTwoTurn = true;
+		}else{
+			playerOneTurn = false;;
+			playerTwoTurn = false;
+			playerOneTurn = true;
 		}
 	}
 
@@ -119,59 +157,170 @@ myApp.controller('gameController',function($scope, $http,$location){
 
 	var updatePosition = function(player, diceTotal){
 		if (player == 1){
-			document.getElementById(playerOnePosition).innerHTML = playerOnePosition;
+			document.getElementById(playerOnePosition).innerHTML = '';
+			if(playerTwoPosition == playerOnePosition){
+				document.getElementById(playerOnePosition).innerHTML = "<img src='../css/images/token-car.png'>";
+			}
 			playerOnePosition += diceTotal;
 			if(playerOnePosition > 39){
 				passGo(1);
 				playerOnePosition -= 40;
 			}
-			document.getElementById(playerOnePosition).innerHTML += 'Player One';
-			$scope.position = "Player One rolled a " + diceTotal;
+			document.getElementById(playerOnePosition).innerHTML += "<img src='../css/images/token-ship.png'>";
 			checkCell(1, playerOnePosition);
 		}else{
-			document.getElementById(playerTwoPosition).innerHTML = playerTwoPosition;
+			document.getElementById(playerTwoPosition).innerHTML = '';
+			if(playerOnePosition==playerTwoPosition){
+				document.getElementById(playerOnePosition).innerHTML += "<img src='../css/images/token-ship.png'>";
+			}
 			playerTwoPosition += diceTotal;
 			if(playerTwoPosition > 39){
 				passGo(2);
 				playerTwoPosition -= 40;
 			}
-			document.getElementById(playerTwoPosition).innerHTML += 'Player Two';
-			$scope.position = "Player Two rolled a " + diceTotal;
+			document.getElementById(playerTwoPosition).innerHTML +="<img src='../css/images/token-car.png'>";
 			checkCell(2, playerTwoPosition);
 		}
 	}
 
 	$scope.rollDice = function(){
+		$scope.specialMessage = false;
+		$scope.playerOneBank = playerOneBank;
+		$scope.playerTwoBank = playerTwoBank;
 		var dice1 = Math.floor(Math.random() * 6 + 1);
+		var imageName1 = "d" + dice1 + ".gif";
+        document.images['dieOne'].src = "css/images/" + imageName1;
+
 		var dice2 = Math.floor(Math.random() * 6 + 1);
-		var diceTotal = dice1 + dice2;
-		if(playerOne){
+		var imageName2 = "d" + dice2 + ".gif";
+        document.images['dieTwo'].src = "css/images/" + imageName2;
+
+		// var diceTotal = dice1 + dice2;
+		var diceTotal =22;
+		//for development
+		if(playerOneTurn == undefined || playerTwoTurn == undefined){
+			playerOneTurn = true;
+		}
+
+		if(playerOneTurn){
 			updatePosition(1,diceTotal);
 		}else{
-			updatePosition(2, diceTotal)
+			updatePosition(2, diceTotal);
 		}
 		changePlayer();
 	}
-
-	var checkCell = function(player, position){
+	$scope.purchaseProperty = function(){
+		$scope.purchaseMessage = " purchased ";
+		cells[$scope.playerPosition].status = "owned";
+		$scope.purchaseButtons = false;
+		document.getElementById("rollButton").disabled = false;
+		if($scope.whichPlayer == 1){
+			playerOneProperties.push(cells[$scope.playerPosition]);
+			document.getElementById($scope.playerPosition).className += " red";
+			playerOneBank -= cells[$scope.playerPosition].price;
+			$scope.playerOneBank = playerOneBank;
+		}else if($scope.whichPlayer == 2){
+			playerTwoProperties.push(cells[$scope.playerPosition]);
+			document.getElementById($scope.playerPosition).className += " blue";
+			playerTwoBank -= cells[$scope.playerPosition].price;
+			$scope.playerTwoBank = playerTwoBank;
+		}
+		checkGroup();
+	}
+	$scope.notPurchase = function(){
+		$scope.purchase = false;
+		$scope.purchaseButtons = false;
+		document.getElementById("rollButton").disabled = false;
+	}
+	var payRent = function(player, position){
 		if(player == 1){
-			console.log(position);
+			playerOneBank -= cells[position].rent;
+			playerTwoBank += cells[position].rent;
 		}else{
-			console.log(position);
+			playerTwoBank -= cells[position].rent;
+			playerOneBank += cells[position].rent;
 		}
 	}
 
+	var checkCell = function(player, position){
+		$scope.rollInfo = true;
+		$scope.playerPosition = position;
+		$scope.whichPlayer = player;
+		$scope.cell = cells[position];
+		if(cells[position].status == "vacant"){
+			$scope.purchaseMessage = " has the option to purchase ";
+			$scope.purchase = true;
+			$scope.purchaseButtons = true;
+			$scope.rent = false;
+			document.getElementById("rollButton").disabled = true;
+		}else if(cells[position].status == "owned"){
+			$scope.purchase = false;
+			$scope.purchaseButtons = false;
+			$scope.rent = true;
+			payRent(player, position);
+		}else if(cells[position].status == "public"){
+			$scope.purchase = false;
+			$scope.purchaseButtons = false;
+			$scope.rent = false;
+			specialSpace(player, cells[position]);
+		}
+	}
 
+	var specialSpace = function(player, position){
+		var position = position.position;
+		var player = player;
+		if(position == 0){
+			//THis is a go
+			//Do nothing
+		}
+		if(position == 2 || position == 17 || position == 33){
+			chestCard(player, position);
+		}
+		if(position == 7 || position == 22 || position == 36){
+			chanceCard(player, position);
 
+		}
+		if(position == 4){
+			if(player == 1){
+				playerOneBank -= Math.floor(playerOneBank * .1);
+			}else{
+				playerTwoBank -= Math.floor(playerTwoBank * .1);
+			}
+			window.message = "Income Tax: Pay 10%";
+		}
+		if(position == 38){
+			if(player == 1){
+				playerOneBank -= 100;
+			}else{
+				playerTwoBank -= 100;
+			}
+			window.message = "Luxury Tax: Pay $100";
+		}
 
+		if(position == 20){
+			//Free Parking
+		}
+		if(position == 30){
+			//Go to jail
+		}
+		if(position == 10){
+			//Visiting Jail
+		}
+		$scope.playerOneBank = playerOneBank;
+		$scope.playerTwoBank = playerTwoBank;
+		$scope.message = window.message;
+		$scope.specialMessage = true;
+		console.log(playerOnePosition);
+
+	}
+
+	var checkGroup = function(){
+		//loop through playerOneProperties/ playerTwo Properties
+		// to check if player has all spots of same group//
+		//if so, do something
+	}
 
 });
-
-
-
-
-
-
 
 
 myApp.controller('infoController',function($scope, $http,$location){
